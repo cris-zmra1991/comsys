3664dd8a2d695fc679506d29b97f736c6f3d43ba
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-10-11 12:32:14 -0400

    Cambios en consulta de registro de contratos

--- views/registrocont.view.php
+++ views/registrocont.view.php
@@ -16,13 +16,13 @@
       </thead>
       <tbody>
         <?php
-        $data = $database->query("SELECT cont.id_fca, prov.desc_proveedor, cont.objeto_cont, cont.vigencia, cont.tipo_vigencia, cont.valor, cont.no_contrato FROM contratos cont INNER JOIN com_proveedores prov ON cont.id_proveedor = prov.id_proveedor");
+        $data = $database->query("SELECT cont.id_fca, prov.desc_proveedor, cont.objeto_cont, cont.vigencia, cont.tipo_vigencia, cont.firma_cont, cont.valor, cont.no_contrato FROM contratos cont INNER JOIN com_proveedores prov ON cont.id_proveedor = prov.id_proveedor");
 
         foreach($data as $solicitudes){ ?>
           <tr>
             <td><?php echo $solicitudes["id_fca"] ?></td>
             <td><?php echo $solicitudes["desc_proveedor"] ?></td>
-            <td><?php echo $solicitudes["objeto"] ?></td>
+            <td><?php echo $solicitudes["objeto_cont"] ?></td>
             <td><?php echo $solicitudes["vigencia"]."&nbsp;".$solicitudes["tipo_vigencia"] ?></td>
             <td><?php echo $solicitudes["firma_cont"] ?></td>
             <td><?php echo $solicitudes["valor"] ?></td>
